b377abfc8569a071cba543c2f66fdd99aa317911
Eduard Canet <ecanet@profeN2I.informatica.escoladeltreball.org>
2019-02-18 13:19:16 +0100

    kerberos

--- k18:kserver/install.sh
+++ k18:kserver/install.sh
@@ -7,10 +7,10 @@ cp kadm5.acl /var/kerberos/krb5kdc/kadm5.acl
 /usr/sbin/kdb5_util create -s -P masterkey
 /usr/sbin/kadmin.local -q "addprinc -pw kpere pere"
 /usr/sbin/kadmin.local -q "addprinc -pw kpau pau"
-/usr/sbin/kadmin.local -q "addprinc -pw kpere pere"
-/usr/sbin/kadmin.local -q "addprinc -pw kpere pere"
-/usr/sbin/kadmin.local -q "addprinc -pw kpere pere"
-/usr/sbin/kadmin.local -q "addprinc -pw kpere pere"
-/usr/sbin/kadmin.local -q "addprinc -pw kpere pere"
-/usr/sbin/kadmin.local -q "addprinc -pw kpere pere"
+/usr/sbin/kadmin.local -q "addprinc -pw kjordi jordi"
+/usr/sbin/kadmin.local -q "addprinc -pw kanna anna"
+/usr/sbin/kadmin.local -q "addprinc -pw kmarta marta"
+/usr/sbin/kadmin.local -q "addprinc -pw kmarta marta/admin"
+/usr/sbin/kadmin.local -q "addprinc -pw kjulia julia"
+/usr/sbin/kadmin.local -q "addprinc -pw ksuperuser superuser"
 
