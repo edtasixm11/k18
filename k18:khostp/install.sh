b51649121171cb5df2171e4a4c167498486ed3c1
edtasixm06 <asixm06@edt.org>
2019-02-22 19:33:22 +0100

    kerberos sshd

--- k18:khostp/install.sh
+++ k18:khostp/install.sh
@@ -1,4 +1,16 @@
 #! /bin/bash
 
-cp /opt/docker/krb5.conf /etc/krb5.conf
+groupadd local01
+groupadd kusers
+useradd -g users -G local01 local01
+useradd -g users -G local01 local02
+useradd -g users -G local01 local03
+useradd -g users -G kusers user01
+useradd -g users -G kusers user02
+useradd -g users -G kusers user03
+echo "local01" | passwd --stdin local01
+echo "local02" | passwd --stdin local02
+echo "local03" | passwd --stdin local03
 
+cp /opt/docker/krb5.conf /etc/krb5.conf
+cp /opt/docker/syste-auth /etc/pam.d/system-auth
